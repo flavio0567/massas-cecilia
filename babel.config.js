3aa4ac2abbe3f899b29bba5dce359720bf8a7c8a
Flavio Rocha <fmrocha@gmail.com>
2020-07-14 20:55:35 -0700

    correcting babel config plugin

--- babel.config.js
+++ babel.config.js
@@ -15,9 +15,9 @@ module.exports = {
         }
       }
     ],
-    'babel-plugin-transform-typescript-metadata',
+    ['babel-plugin-transform-typescript-metadata', { legacy: true }],
     ['@babel/plugin-transform-typescript', { allowNamespaces: true }],
-    [('@babel/plugin-proposal-decorators', { legacy: true })],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }]
   ]
 };
